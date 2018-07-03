74cde068b5d0f70bc54fba8992b939d5d351a47c
Yuriev Dmitriy <yurievdmitriy1989@gmail.com>
2018-07-03 13:22:31 +0300

    Was modified LastViewProductsManager

--- src/Service/LastViewProductsManager.php
+++ src/Service/LastViewProductsManager.php
@@ -12,42 +12,40 @@ class LastViewProductsManager
 
     private $session;
     private $repository;
+    private $lastView;
 
     public function __construct(SessionInterface $session, ProductRepository $repository)
     {
         $this->session = $session;
         $this->repository = $repository;
+        $this->lastView = $session->get(self::SESSION_LAST_PRODUCT_ID);
     }
 
     public function addLastViewProducts(Product $product)
     {
-        $lastView = $this->session->get(self::SESSION_LAST_PRODUCT_ID);
-
-        if (empty($lastView)) {
-            $lastView[] = $product->getId();
-            $this->session->set(self::SESSION_LAST_PRODUCT_ID, $lastView);
+        if (empty($this->lastView)) {
+            $this->lastView[] = $product->getId();
+            $this->session->set(self::SESSION_LAST_PRODUCT_ID, $this->lastView);
 
             return;
         }
 
-        if(!(in_array($product->getId(), $lastView))){
-            $lastView[] = $product->getId();
-            $this->session->set(self::SESSION_LAST_PRODUCT_ID, $lastView);
+        if (!(in_array($product->getId(), $this->lastView))){
+            $this->lastView[] = $product->getId();
+            $this->session->set(self::SESSION_LAST_PRODUCT_ID, $this->lastView);
         }
     }
 
-    public function getLastViewProducts()
+    public function getLastViewProducts() :array
     {
         $res = [];
-        $lastView = $this->session->get(self::SESSION_LAST_PRODUCT_ID);
 
-        if ($this->session->has(self::SESSION_LAST_PRODUCT_ID) && isset($lastView)) {
-            foreach ($lastView as $productId) {
+        if ($this->session->has(self::SESSION_LAST_PRODUCT_ID) && isset($this->lastView)) {
+            foreach ($this->lastView as $productId) {
                 $res[] = $this->repository->find($productId);
             }
         }
 
         return $res;
     }
-    
 }
